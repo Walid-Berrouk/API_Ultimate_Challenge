45c05a3f999b41354141b8dadb412f420acc4b46
Walid Berrouk <jw_berrouk@esi.dz>
2022-09-24 15:30:23 +0100

    Integrating Features Section

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+
+import axios from 'axios'
+
+import { baseUrl } from '../shared/baseUrl'
 
 
 import { FaRegUserCircle } from 'react-icons/fa'
@@ -8,42 +12,67 @@ import { BsToggles2, BsCollection } from 'react-icons/bs'
 
 
 export default function Features() {
+
+  const [features, setFeatures ] = useState({
+    isLoading: true,
+    content: [],
+    err: ''
+  })
+
+  useEffect(() => {
+
+    axios
+    .get(`${baseUrl}/features`)
+    .then(res => setFeatures({
+      ...features,
+      isLoading: false,
+      content:res.data,
+    }))
+    .catch(err => setFeatures({
+      ...features,
+      isLoading: false,
+      err: err.message
+    }))
+
+  }, [])
+
+  const images = [
+    <BsCollection size={'4rem'} className="text-white inline-block bg-primary bg-gradient p-3 rounded mb-3"/>,
+    <FaRegUserCircle size={'4rem'} className="text-white inline-block bg-primary bg-gradient p-3 rounded mb-3"/>,
+    <BsToggles2 size={'4rem'} className="text-white inline-block bg-primary bg-gradient p-3 rounded mb-3"/>
+  ]
+
+  const featuresList = features.content.map((feature, index) => (
+    <div key={feature.id} className="feature col">
+        <div className="">
+          {images[index]}
+        </div>
+        <h2>{feature.title}</h2>
+        <p>{feature.description}</p>
+        <a href={feature.link} className="icon-link">
+          Call to action
+          <FiChevronRight />
+        </a>
+      </div>
+  ))
+
   return (
     <div className='container'>
         <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
-          <div className="feature col">
-            <div className="">
-              <BsCollection size={'4rem'} className="text-white inline-block bg-primary bg-gradient p-3 rounded mb-3"/>
-            </div>
-            <h2>Featured title</h2>
-            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-            <a href="#" className="icon-link">
-              Call to action
-              <FiChevronRight />
-            </a>
-          </div>
-          <div className="feature col">
-            <div className="">
-              <FaRegUserCircle size={'4rem'} className="text-white inline-block bg-primary bg-gradient p-3 rounded mb-3"/>
-            </div>
-            <h2>Featured title</h2>
-            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-            <a href="#" className="icon-link">
-              Call to action
-              <FiChevronRight />
-            </a>
-          </div>
-          <div className="feature col">
-          <div className="">
-              <BsToggles2 size={'4rem'} className="text-white inline-block bg-primary bg-gradient p-3 rounded mb-3"/>
-            </div>
-            <h2>Featured title</h2>
-            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-            <a href="#" className="icon-link">
-              Call to action
-              <FiChevronRight />
-            </a>
-          </div>
+        {features.isLoading ?
+              <div className='m-5 d-flex justify-content-center'>
+                <div className="spinner-border" role="status">
+                </div>
+              </div>
+              : !features.err ?
+                    <>
+                      {featuresList}
+                    </>
+                  :
+                    <div className="alert alert-danger" role="alert">
+                      Error : {features.err}
+                      </div>
+          }
         </div>
     </div>
   )
