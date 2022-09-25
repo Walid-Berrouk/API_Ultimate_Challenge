b3c132f76fb0f6b09715142a22b9ac41c5f199a2
Walid Berrouk <jw_berrouk@esi.dz>
2022-09-25 08:44:48 +0100

    Integrating Sign Up form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <div className="container">
     <header className="d-flex justify-content-center py-3">
       <ul className="nav nav-pills">
-        <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
+        <li className="nav-item"><Link to='/main/home' className="nav-link active">Home</Link></li>
         <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
         <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
         <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,41 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { baseUrl } from '../shared/baseUrl';
 
 export default function SignUp() {
+
+  const [account, setAccount] = useState({
+    firstname: "",
+    lastname: "",
+    job: "",
+    age: "",
+    codeName: "",
+    email: "",
+    password: "",
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+    .post(`${baseUrl}/profiles`,
+      {
+        body: {
+            profile: account
+        }
+      }
+    )
+    // Take a look at the console and see the res.data attribute
+    .then(res => {
+      console.log(res)
+      alert(res.statusText)
+    })
+    .catch(error => {
+      console.log(error)
+      alert(error.message)
+    })
+
+  }
+
   return (
     <div className="container col-xl-10 col-xxl-8 px-4 py-5">
     <div className="row align-items-center g-lg-5 py-5">
@@ -9,34 +44,41 @@ export default function SignUp() {
         <p className="col-lg-10 fs-4">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
       </div>
       <div className="col-md-10 mx-auto col-lg-5">
-        <form className="p-4 p-md-5 border rounded-3 bg-light">
+        <form className="p-4 p-md-5 border rounded-3 bg-light" onSubmit={handleSubmit}>
           <div className="form-floating mb-3">
-            <input type="text" className="form-control" id="floatingInput" placeholder="John" />
-            <label for="floatingInput">First Name</label>
+            <input type="text" className="form-control" id="floatingInput" placeholder="John"
+                  value={account.firstname} onChange={(event) => setAccount({...account, firstname: event.target.value})}/>
+            <label htmlFor="floatingInput">First Name</label>
           </div>
           <div className="form-floating mb-3">
-            <input type="text" className="form-control" id="floatingInput" placeholder="Doe" />
-            <label for="floatingInput">Last Name</label>
+            <input type="text" className="form-control" id="floatingInput" placeholder="Doe"
+                  value={account.lastname} onChange={(event) => setAccount({...account, lastname: event.target.value})} />
+            <label htmlFor="floatingInput">Last Name</label>
           </div>
           <div className="form-floating mb-3">
-            <input type="text" className="form-control" id="floatingInput" placeholder="Doe" />
-            <label for="floatingInput">Job</label>
+            <input type="text" className="form-control" id="floatingInput" placeholder="Doe"
+                  value={account.job} onChange={(event) => setAccount({...account, job: event.target.value})} />
+            <label htmlFor="floatingInput">Job</label>
           </div>
           <div className="form-floating mb-3">
-            <input type="number" className="form-control" id="floatingInput" placeholder="Doe" />
-            <label for="floatingInput">Age</label>
+            <input type="number" className="form-control" id="floatingInput" placeholder="Doe" 
+                  value={account.age} onChange={(event) => setAccount({...account, age: event.target.value})}/>
+            <label htmlFor="floatingInput">Age</label>
           </div>
           <div className="form-floating mb-3">
-            <input type="text" className="form-control" id="floatingInput" placeholder="Doe" />
-            <label for="floatingInput">Code Name</label>
+            <input type="text" className="form-control" id="floatingInput" placeholder="Doe"
+                  value={account.codeName} onChange={(event) => setAccount({...account, codeName: event.target.value})} />
+            <label htmlFor="floatingInput">Code Name</label>
           </div>
           <div className="form-floating mb-3">
-            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-            <label for="floatingInput">Email address</label>
+            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"
+                  value={account.email} onChange={(event) => setAccount({...account, email: event.target.value})} />
+            <label htmlFor="floatingInput">Email address</label>
           </div>
           <div className="form-floating mb-3">
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-            <label for="floatingPassword">Password</label>
+            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" 
+                  value={account.password} onChange={(event) => setAccount({...account, password: event.target.value})} />
+            <label htmlFor="floatingPassword">Password</label>
           </div>
           <div className="checkbox mb-3">
             <label>
